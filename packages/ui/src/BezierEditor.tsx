1bf07b1d2af36b706c9284e0e60aa116451cc657
fauriatjules-max <fauriatjules@gmail.com>
2025-11-29 16:05:38 +0100

    Create BezierEditor.tsx

---
+++ packages/ui/src/BezierEditor.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface BezierEditorProps {
+  // Props pour l'éditeur de courbes de Bézier
+}
+
+export const BezierEditor: React.FC<BezierEditorProps> = (props) => {
+  return (
+    <div className="bezier-editor">
+      Éditeur de courbes de Bézier (à implémenter)
+    </div>
+  );
+};
